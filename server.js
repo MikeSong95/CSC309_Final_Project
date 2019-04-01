f5880504a5f2815a47aecd4f1da1215a98edaafb
Seong Kim <hoon5421@hotmail.com>
2019-04-01 12:50:58 -0400

    Adding .gitignore

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const log = console.log;
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
+const path = require('path');
 
 // Mongoose
-const { mongoose } = require('./db/mongoose');
-const { Patient } = require('./models/patient')
+//const { mongoose } = require('./db/mongoose');
+//const { Patient } = require('./models/patient')
 
 // Express
 const port = process.env.PORT || 3000
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+});
