032ef1bc43a204b37216a6f822ca715139447317
Mike Song <mike.song@mail.utoronto.ca>
2019-04-04 22:01:51 -0400

    bcrypt -> bcryptjs

--- models/doctor.js
+++ models/doctor.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const AppointmentModel = require('./appointment');
 const NotificationModel = require('./notification');
 const PatientModel = require('./patient');
