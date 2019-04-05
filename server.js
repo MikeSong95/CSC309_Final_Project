8b09d756c118089db12fd9ddc24bcda43ef9dad7
Michael Song <mike.song@mail.utoronto.ca>
2019-04-04 20:28:58 -0400

    local server.js:

--- server.js
+++ server.js
@@ -81,7 +81,6 @@ app.get("/patient-dashboard", (req,res) => {
 	} else {
 		email = req.body.email
 	}
-
 	// check if we have active session cookie
 	if (req.session.user) {
 		res.render('patient-dashboard', {
@@ -140,7 +139,6 @@ app.get("/patient-edit-profile", (req, res) => {
 });
 
 app.post("/edit-patient", (req, res) => {
-	console.log("Editing patient")
 	const data = {
 		password: req.body.password,
 		email: req.body.email, 
@@ -150,54 +148,59 @@ app.post("/edit-patient", (req, res) => {
 		gender:req.body.gender,
 		hcNum: req.body.hcnum
 	}
-	
+
 	// Otherwise, findById
 	Patient.findOne({email:req.session.email}).then((patient) => {
 		if (!patient) {
 			res.status(404).send();
 		} else {
-			if (data.password != "") {
+			if (data.password != undefined) {
+				console.log("modified password");
 				patient.password = data.password;		
 				// Mark it as modified
 				patient.markModified('password');
 			} 
-			if (data.email != "") {
+			if (data.email != undefined) {
 				patient.email = data.email;
 				// Mark it as modified
 				patient.markModified('email');
 			}
-			if (data.phoneNum != "") {
+			if (data.phoneNum != undefined) {
 				patient.phoneNum = data.phoneNum;
 				// Mark it as modified
 				patient.markModified('phoneNum');
 			}
-			if (data.fName != "") {
+			if (data.fName != undefined) {
 				patient.fName = data.fName;
 				// Mark it as modified
 				patient.markModified('fName');
 			}
-			if (data.lName != "") {
+			if (data.lName != undefined) {
 				patient.lName = data.lName;
 				// Mark it as modified
 				patient.markModified('lName');
 			}
-			if (data.gender != "") {
+			if (data.gender != undefined) {
 				patient.gender = data.gender;
 				// Mark it as modified
 				patient.markModified('gender');
 			}
-			if (data.hcNum != "") {
+			if (data.hcNum != undefined) {
 				patient.hcNum = data.hcNum;
 				// Mark it as modified
 				patient.markModified('hcNum');
 			}
 
 			// Save it
-			patient.save().then(() => {
-				res.redirect('/patient-dashboard?email=' + req.session.email)
-			}, (error) => {
-				res.status(400).send(error) // 400 for bad request
-			})
+			patient.save(
+				function(err, result) {
+					if (err) {
+						// An error occurred, stop execution and return 500
+						return res.status(500).send();
+					} else {
+						res.redirect('/patient-dashboard?email=' + req.session.email)
+					}
+				});
 		}
 	}).catch((error) => {
 		res.status(500).send();
