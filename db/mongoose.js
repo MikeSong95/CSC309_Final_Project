d07a9adc59fd5e87a2daac3a7051f9aa8cac97c2
Michael Song <mike.song@mail.utoronto.ca>
2019-04-04 21:19:25 -0400

    local mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_5xc2vgqp:621t31ltt3mdmibfimoo2nh9ae@ds211096.mlab.com:11096/heroku_5xc2vgqp', { useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MedAPI', { useNewUrlParser: true, useCreateIndex: true});
 
 module.exports = {
 	mongoose
