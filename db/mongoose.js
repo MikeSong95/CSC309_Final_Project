89938b83192cff1e9300256aa27117f477cb5642
Jacky Fong <jacky.fong@mail.utoronto.ca>
2019-04-04 21:22:37 -0400

    Minor schema change to relax the email validation

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
+//const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/MedAPI'
+// mongoose.connect(mongoURI, { useNewUrlParser: true});
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_5xc2vgqp:621t31ltt3mdmibfimoo2nh9ae@ds211096.mlab.com:11096/heroku_5xc2vgqp', { useNewUrlParser: true, useCreateIndex: true});
 
